9c770351a5dfbbe6a65f73d12751c043060aa531
brandonjacobs1 <brandon.jacobs345@gmail.com>
2023-04-10 09:19:22 -0600

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace fag_el_gamous.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
