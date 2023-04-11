4a903c519ad1ede77935fdd1210147ab54ea7ebd
ctloar <cl464@byu.edu>
2023-04-11 17:56:56 -0600

    Hide admin page from unauthenticated users

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace fag_el_gamous.Data;
@@ -8,6 +10,30 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+
     }
+
+    //public DbSet<IdentityRole> Roles { get; set; } = null!;
+
+    //protected override void OnModelCreating(ModelBuilder mb)
+    //{
+    //    mb.Entity<IdentityRole>().HasData
+    //        (
+    //            new IdentityRole
+    //            {
+    //                Id = "1",
+    //                ConcurrencyStamp = "4/11/2023",
+    //                Name = "Admin",
+    //                NormalizedName = "Admin"
+    //            },
+    //            new IdentityRole
+    //            {
+    //                Id = "2",
+    //                ConcurrencyStamp = "4/11/2023",
+    //                Name = "Researcher",
+    //                NormalizedName = "Researcher"
+    //            }
+    //        );
+    //}
 }
 
