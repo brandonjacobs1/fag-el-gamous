c9232445c4dc01475749ced1af63130864c79eab
ctloar <cl464@byu.edu>
2023-04-13 16:28:04 -0600

    Admin changes and accessing photo data urls

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -30,21 +30,46 @@ namespace fag_el_gamous.Controllers
         public async Task<IActionResult> Admin()
         {
             var users = _userManager.Users.ToList();
-            var viewModel = new List<UserRolesView>(); // Create a list to hold the view model objects
+            var viewModel = new List<UserRolesView>(); 
 
             foreach (var user in users)
             {
-                var userRoles = await _userManager.GetRolesAsync(user); // Get the roles for the current user
+                var userRoles = await _userManager.GetRolesAsync(user); 
                 var userRoleViewModel = new UserRolesView
                 {
                     User = user,
                     Roles = userRoles
                 };
-                viewModel.Add(userRoleViewModel); // Add the view model object to the list
+                viewModel.Add(userRoleViewModel); 
             }
 
-            return View(viewModel); // Pass the list of view models to the view
+            return View(viewModel); 
         }
+
+
+        public async Task<IActionResult> ManageRoles()
+        {
+            //var users = _userManager.Users.ToList();
+            var roles = _roleManager.Roles.ToList();
+            //var viewModel = new List<UserRolesView>();
+
+            //foreach (var user in users)
+            //{
+            //    var userRoles = await _userManager.GetRolesAsync(user);
+            //    var userRoleViewModel = new UserRolesView
+            //    {
+            //        User = user,
+            //        Roles = userRoles
+            //    };
+            //    viewModel.Add(userRoleViewModel);
+            var viewModel = new UserRolesView
+            {
+                ListRoles = roles
+            };
+
+            return View(viewModel);
+        }
+
 
         [HttpGet]
         public async Task<IActionResult> Edit(string id)
@@ -56,7 +81,7 @@ namespace fag_el_gamous.Controllers
 
             var user = await _userManager.FindByIdAsync(id);
             var roles = _roleManager.Roles.ToList();
-            var userRoles = await _userManager.GetRolesAsync(user); // Get the roles for the current user
+            var userRoles = await _userManager.GetRolesAsync(user); 
             
 
             var viewModel = new UserRolesView
@@ -72,15 +97,18 @@ namespace fag_el_gamous.Controllers
             }
             return View(viewModel);
         }
+
+
         [HttpGet]
-        public async Task<IActionResult> Delete(string id, string roleName)
+        public async Task<IActionResult> Delete(string? id, string? roleName)
         {
-            if (id == null || _userManager.Users == null)
+            if (id == null && _userManager.Users == null)
             {
                 return NotFound();
             }
 
             var user = await _userManager.FindByIdAsync(id);
+            var role = await _roleManager.FindByNameAsync(roleName);
             //var role = _roleManager.FindByNameAsync(roleName);
             //var userRoles = await _userManager.GetRolesAsync(user); // Get the roles for the current user
 
@@ -88,15 +116,42 @@ namespace fag_el_gamous.Controllers
             var viewModel = new UserRolesView
             {
                 User = user,
-                NewRole = roleName
+                NewRole = roleName,
+                SingleRole = role
             };
 
-            if (user == null)
+            //if (user == null)
+            //{
+            //    return NotFound();
+            //}
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmRoleDelete(string roleName)
+        {
+            if (roleName == null)
             {
                 return NotFound();
             }
+
+            var role = await _roleManager.FindByNameAsync(roleName);
+            //var role = _roleManager.FindByNameAsync(roleName);
+            //var userRoles = await _userManager.GetRolesAsync(user); // Get the roles for the current user
+
+
+            var viewModel = new UserRolesView
+            {
+                SingleRole = role
+            };
+
+            //if (user == null)
+            //{
+            //    return NotFound();
+            //}
             return View(viewModel);
         }
+
 
 
         [HttpPost]
@@ -113,7 +168,7 @@ namespace fag_el_gamous.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Admin");
+                    return RedirectToAction("ManageRoles");
                 }
                 else
                 {
@@ -121,7 +176,33 @@ namespace fag_el_gamous.Controllers
                 }
             }
 
-            return RedirectToAction("Admin");
+            return RedirectToAction("ManageRoles");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string roleId)
+        {
+            if (roleId != null)
+            {
+                //var role = new IdentityRole
+                //{
+                //    Name = model.
+                //};
+                var role = await _roleManager.FindByIdAsync(roleId);
+                var result = await _roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ManageRoles");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "failed to add role");
+                }
+            }
+
+            return RedirectToAction("ManageRoles");
         }
 
 
